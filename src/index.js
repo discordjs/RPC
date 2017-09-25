9d0f8e2d3723c2a4e9888156cdd07f7af869efca
Gus Caplan <me@gus.host>
2017-09-25 10:37:11 -0500

    initial docs

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const errors = require('discord.js/src/errors');
-errors.register('RPC_INVALID_TRANSPORT', (name) => `Invalid transport: ${name}`);
-
-module.exports = {
-  Client: require('./Client'),
-};
--- src/transports/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  ipc: require('./IPC'),
-  websocket: require('./WebSocket'),
-};
