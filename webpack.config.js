918c5cd59165434cebd1de6e3238f3b3ded71410
Gus Caplan <me@gus.host>
2018-06-28 19:33:03 -0500

    update webpack process

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,12 @@
 'use strict';
 
-const path = require('path');
-const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-const plugins = [
-  new webpack.optimize.ModuleConcatenationPlugin(),
-];
-
-const prod = process.env.NODE_ENV === 'production';
-
-if (prod) {
-  plugins.push(new UglifyJSPlugin({
-    uglifyOptions: {
-      mangle: { keep_classnames: true },
-      output: { comments: false },
-    },
-  }));
-}
-
 module.exports = {
-  entry: './src/index.js',
+  mode: 'production',
+  entry: require.resolve('.'),
   output: {
-    path: path.resolve('.'),
+    path: __dirname,
     filename: 'browser.js',
-    library: 'DiscordRPC',
+    library: 'Snekfetch',
     libraryTarget: 'umd',
   },
-  module: {
-    rules: [
-      { test: /\.md$/, loader: 'ignore-loader' },
-    ],
-  },
-  node: {
-    fs: 'empty',
-    dns: 'empty',
-    tls: 'empty',
-    child_process: 'empty',
-    dgram: 'empty',
-    __dirname: true,
-    process: false,
-    path: 'empty',
-    Buffer: false,
-    zlib: 'empty',
-  },
-  plugins,
 };
